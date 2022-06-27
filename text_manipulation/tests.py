f060dafad7899b6c75c964f99b7f18fdda17e615
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2022-06-26 19:08:44 -0700

    cleaned up the code, added todos

--- text_manipulation/tests.py
+++ text_manipulation/tests.py
@@ -1,5 +1,4 @@
 import unittest
-import os
 
 from yaml_to_json_file_utils import get_dict_object_from_yaml_file, write_yaml_dict_to_json_file
 
@@ -9,7 +8,9 @@ class TestYamlFunctions(unittest.TestCase):
     test_yaml_file_path = './test_files/test.yaml'
     expected_json_file_path = './test_files/expected_json.json'
     actual_json_file_path = './test_files/actual_json.json'
-    test_yaml_dict = {
+
+    def test_yaml_loader(self):
+        expected_dict = {
             'doe': 'a deer, a female deer',
             'ray': 'a drop of golden sun',
             'pi': 3.14159,
@@ -17,14 +18,19 @@ class TestYamlFunctions(unittest.TestCase):
             'french-hens': 3,
             'calling-birds': ['huey', 'dewey', 'louie', 'fred']
         }
-
-    def test_yaml_loader(self):
         actual_dict = get_dict_object_from_yaml_file(self.test_yaml_file_path)
 
-        self.assertEqual(self.test_yaml_dict, actual_dict)
+        self.assertEqual(expected_dict, actual_dict)
 
     def test_conversion_write_to_json(self):
-        write_yaml_dict_to_json_file(self.test_yaml_dict, 'test_files/actual_json')
+        write_yaml_dict_to_json_file({
+            'doe': 'a deer, a female deer',
+            'ray': 'a drop of golden sun',
+            'pi': 3.14159,
+            'xmas': True,
+            'french-hens': 3,
+            'calling-birds': ['huey', 'dewey', 'louie', 'fred']
+        }, 'test_files/actual_json')
         with open(self.expected_json_file_path, 'r') as expected_json_file, \
                 open(self.actual_json_file_path, 'r') as actual_json_file:
             expected_json_string = expected_json_file.read()
--- text_manipulation/yaml_to_json_file_utils.py
+++ text_manipulation/yaml_to_json_file_utils.py
@@ -21,13 +21,6 @@ def write_json_string_to_file_with_name(json_string: str, file_name: str) -> Non
         json.dump(json_string, json_file, default=lambda o: o.__dict__, sort_keys=True, indent=2)
 
 
-if __name__ == '__main__':
-    print(get_dict_object_from_yaml_file('./test_files/test.yaml'))
-    print(write_yaml_dict_to_json_file({
-            'doe': 'a deer, a female deer',
-            'ray': 'a drop of golden sun',
-            'pi': 3.14159,
-            'xmas': True,
-            'french-hens': 3,
-            'calling-birds': ['huey', 'dewey', 'louie', 'fred']
-        }, 'converted_json'))
+# TODO: Create file deletion/replacement function
+# TODO: Create directory crawler
+# TODO: Create command line interface
